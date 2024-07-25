06e2b6ac47d6a1ba2b2ad36d4ac2f7515a1da0d0
Jasen Tenney <jasen_tenney@hotmail.com>
2024-07-25 13:17:40 -0500

    Linear search, binary search and two crystal balls search

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,19 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpBy = Math.floor(Math.sqrt(breaks.length));
+    let i = jumpBy;
+    let found = false;
 
+    for (; i < breaks.length; i += jumpBy) {
+        //use first crystal ball to find a point where it breaks
+        if (breaks[i]) {
+            // back up by sqrt amount and start checking incrementally
+            i -= jumpBy;
+            while (!breaks[i] && i < breaks.length) {
+                i++;
+            }
+            return i;
+        }
+    }
+
+    return -1;
 }
